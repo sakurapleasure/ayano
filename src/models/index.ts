de62e14e7243c16ff0be015398204855268fcc9f
petitsurume <gamemannew2013@yahoo.co.jp>
2016-12-31 10:43:26 +0900

    add category model

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,6 +5,8 @@ mongoose.connect(config["mongo_url"])
 
 import user from './user'
 import post from './post'
+import category from './category'
 
 export const users = user()
 export const posts = post()
+export const categories = category()
