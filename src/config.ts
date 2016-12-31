d9aa3d2e2358a11d2a965b533f43478300b98aae
petitsurume <gamemannew2013@yahoo.co.jp>
2016-12-31 20:01:26 +0900

    add secret_key

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,17 @@
 import readline = require("readline-sync")
 import fs = require("fs")
+import rndstr from "rndstr"
 
 const schemas = [
     {
         name: "mongo_url",
         type: "string",
         question_text: "MongoDBの接続情報を入力してください(例: mongodb://localhost:27017/ayano)"
+    },
+    {
+        name: "secret_key",
+        type: "random",
+        question_text: false
     }
 ]
 
@@ -19,6 +25,14 @@ config = config || {}
 var updateFlag = false
 schemas.forEach(function(schema){
     if(config[schema.name]) return
+    if(schema.type === "random"){
+        config[schema.name] = rndstr({
+            length:64,
+            chars:"0-9a-zA-Z"
+        })
+        updateFlag = true
+        return
+    }
     console.log(schema.name)
     var answer = readline.question(schema.question_text+" > ")
     config[schema.name] = answer
